30f90ae0ee08c75b7e727d42b662270aa41855e3
Hammad <hammadhawc227@gmail.com>
2022-11-27 03:54:32 -0800

    02 (Variables): Annotating Strings.

---
+++ 01-type-annotations/variables.ts
@@ -0,0 +1,16 @@
+// Annotating Primitive Types
+// string, number, boolean, null, undefined
+
+// 1. Strings
+
+let movieTitle: string = 'In Time'
+movieTitle = 'Dark Knight'
+
+// It is illegal in TypeScript, but it is totally valid in JavaScript.
+
+// movieTitle = 20
+
+// Accessing the property that does not exists in the string type will throws
+// an error.
+
+// movie.upper()
