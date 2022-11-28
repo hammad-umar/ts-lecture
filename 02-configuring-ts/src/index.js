e5faccb0eb0539f177d81cc749281da0559b021f
Hammad <hammadhawc227@gmail.com>
2022-11-28 10:23:32 -0800

    03 (TS Config Settings) include and exclude options in tsconfig.json.

--- 02-configuring-ts/src/index.js
+++ 02-configuring-ts/src/index.js
@@ -1,21 +1,16 @@
-interface Chicken {
-  name: string
-  breed: string
-  eggsPerWeek: number
-  age: number
-}
-
-const norma: Chicken = {
-  name: 'Norma',
-  breed: 'Silkie',
-  eggsPerWeek: 4,
-  age: 0.3,
-}
-
+"use strict";
+const norma = {
+    name: 'Norma',
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    age: 0.3,
+};
 // TypeScript Compiler Option (Watch Mode).
 // -> Watch mode listen for changes we make in a file.
 // -> COMMAND: tsc --watch <filename>.ts or tsc -w <filename>.ts
-
 // "files" option in tsconfig.json
 // -> We use it to tell the compiler that we only want to compile these files.
 // -> If provided file in the list does not exist then compiler throws an error.
+// "include" and "exclude" option in tsconfig.json
+// -> In "include" we specify array of files or patterns we want to compile.
+// -> In "excluide" we specify array of files or patterns we don't want to compile.
