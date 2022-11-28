4747fe0276b4087806be27da9180a9a41172fdab
Hammad <hammadhawc227@gmail.com>
2022-11-28 03:14:43 -0800

    02 (Enums): Introducing Enum Types.

--- 01-type-annotations/tuples-and-enums.ts
+++ 01-type-annotations/tuples-and-enums.ts
@@ -15,3 +15,37 @@ const responses: HTTPResponse[] = [
   [404, 'Not Found!'],
   [500, 'Internal Server Error!'],
 ]
+
+// Enum Types.
+// -> Enum type is also exclusive to TypeScript (Not in JS).
+// -> Enums allow us to define a set of named constants.
+// -> We can give these constants numeric or string values.
+
+// -> If we can't assign values to enums then TypeScript automatically
+// give them numeric values
+
+enum OrderStatus {
+  PENDING, // 1
+  DELIVERED, // 2
+  RETURNED, // 3
+}
+
+const myOrder = {
+  orderNumber: 1263735,
+  status: OrderStatus.DELIVERED,
+}
+
+// -> We can also give them values.
+
+enum ArrowKeys {
+  UP = 'UP',
+  DOWN = 'DOWN',
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
+
+const move = ArrowKeys.LEFT
+
+if (move === ArrowKeys.LEFT) {
+  // do something.
+}
