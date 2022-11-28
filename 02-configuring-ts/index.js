f226e0bb4c3866f81303b65b2e6a2379470c9d17
Hammad <hammadhawc227@gmail.com>
2022-11-28 09:55:07 -0800

    03 (TS Config Settings) Watch Mode Option for TS compiler.

--- 02-configuring-ts/index.js
+++ 02-configuring-ts/index.js
@@ -1,5 +1,9 @@
 var norma = {
     name: 'Norma',
     breed: 'Silkie',
-    eggsPerWeek: 4
+    eggsPerWeek: 4,
+    age: 0.3
 };
+// TypeScript Compiler Option (Watch Mode).
+// -> Watch mode listen for changes we make in a file.
+// -> COMMAND: tsc --watch <filename>.ts or tsc -w <filename>.ts
--- 02-configuring-ts/index.ts
+++ 02-configuring-ts/index.ts
@@ -2,10 +2,16 @@ interface Chicken {
   name: string
   breed: string
   eggsPerWeek: number
+  age: number
 }
 
 const norma: Chicken = {
   name: 'Norma',
   breed: 'Silkie',
   eggsPerWeek: 4,
+  age: 0.3,
 }
+
+// TypeScript Compiler Option (Watch Mode).
+// -> Watch mode listen for changes we make in a file.
+// -> COMMAND: tsc --watch <filename>.ts or tsc -w <filename>.ts
