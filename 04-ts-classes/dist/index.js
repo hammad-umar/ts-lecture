30f82c6162bfabf7f208b7056174e6a9de0782d1
Hammad <hammadhawc227@gmail.com>
2022-11-29 07:46:43 -0800

    05 (TS Classes): Class Fields.

--- 04-ts-classes/dist/index.js
+++ 04-ts-classes/dist/index.js
@@ -4,6 +4,11 @@ console.log('TS CLASSES!');
 // -> Annotating a TypeScript class.
 class Player {
     constructor(first, last) {
+        // Class Fields
+        // -> We can also rely on type inference in a class when are defining a field
+        // -> When a field is not known by a TypeScript then we annotate it.
+        // -> We can also define a field like this and hardcode a value to it.
+        this.score = 0;
         this.first = first;
         this.last = last;
     }
--- 04-ts-classes/src/index.ts
+++ 04-ts-classes/src/index.ts
@@ -7,6 +7,12 @@ class Player {
   first: string
   last: string
 
+  // Class Fields
+  // -> We can also rely on type inference in a class when are defining a field
+  // -> When a field is not known by a TypeScript then we annotate it.
+  // -> We can also define a field like this and hardcode a value to it.
+  score: number = 0
+
   constructor(first: string, last: string) {
     this.first = first
     this.last = last
