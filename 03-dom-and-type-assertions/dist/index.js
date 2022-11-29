2bacb83475397377d510ecd486443db07dba9d10
Hammad <hammadhawc227@gmail.com>
2022-11-29 00:58:05 -0800

    04 (DOM and Assertions) Type Assertions.

--- 03-dom-and-type-assertions/dist/index.js
+++ 03-dom-and-type-assertions/dist/index.js
@@ -5,3 +5,9 @@ console.log(btn);
 btn.addEventListener('click', () => {
     alert('CLICKED');
 });
+// Type Assertions.
+// -> Sometimes we know more than TypeScript about a value type.
+// -> And we make sure TypeScript knows it too.
+// -> We can assert a value's type using "as" keyword.
+const mystery = 'SOME MYSTRY!';
+const strLength = mystery.length;
--- 03-dom-and-type-assertions/src/index.ts
+++ 03-dom-and-type-assertions/src/index.ts
@@ -6,3 +6,12 @@ console.log(btn)
 btn.addEventListener('click', () => {
   alert('CLICKED')
 })
+
+// Type Assertions.
+// -> Sometimes we know more than TypeScript about a value type.
+// -> And we make sure TypeScript knows it too.
+// -> We can assert a value's type using "as" keyword.
+
+const mystery: unknown = 'SOME MYSTRY!'
+
+const strLength = (mystery as string).length
