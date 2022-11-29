2caebe158043178e5133a158a671887593a544f5
Hammad <hammadhawc227@gmail.com>
2022-11-29 07:41:31 -0800

    05 (TS Classes): Annotating TypeScript Clasess.

--- 04-ts-classes/dist/index.js
+++ 04-ts-classes/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
 console.log('TS CLASSES!');
+// TypeScript Classes.
+// -> Annotating a TypeScript class.
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+const player = new Player('A', 'B');
--- 04-ts-classes/src/index.ts
+++ 04-ts-classes/src/index.ts
@@ -1,1 +1,16 @@
 console.log('TS CLASSES!')
+
+// TypeScript Classes.
+// -> Annotating a TypeScript class.
+
+class Player {
+  first: string
+  last: string
+
+  constructor(first: string, last: string) {
+    this.first = first
+    this.last = last
+  }
+}
+
+const player = new Player('A', 'B')
