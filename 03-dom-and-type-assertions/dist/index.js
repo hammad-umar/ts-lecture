e0def2dce1bf7aa497268d7f11c78cb8f596fed0
Hammad <hammadhawc227@gmail.com>
2022-11-29 01:34:40 -0800

    04 (DOM and Assertions) Adding the Interface.

--- 03-dom-and-type-assertions/dist/index.js
+++ 03-dom-and-type-assertions/dist/index.js
@@ -1,16 +1,24 @@
 "use strict";
+const todos = [];
 const inputEl = document.getElementById('input');
 const todoFormEl = document.getElementById('todoForm');
 const todoListContainerEl = document.getElementById('todoList');
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const todoText = inputEl.value;
+const createTodo = (todo) => {
     const todoItemEl = document.createElement('li');
-    todoItemEl.textContent = todoText;
+    todoItemEl.textContent = todo.text;
     const todoItemCheckboxEl = document.createElement('input');
     todoItemCheckboxEl.type = 'checkbox';
     todoListContainerEl.appendChild(todoItemEl);
     todoListContainerEl.appendChild(todoItemCheckboxEl);
+};
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+        id: new Date().getTime(),
+        text: inputEl.value,
+        isCompleted: false,
+    };
+    createTodo(newTodo);
     inputEl.value = '';
 };
 todoFormEl.addEventListener('submit', handleSubmit);
--- 03-dom-and-type-assertions/src/index.ts
+++ 03-dom-and-type-assertions/src/index.ts
@@ -1,22 +1,38 @@
+interface Todo {
+  id: number
+  text: string
+  isCompleted: boolean
+}
+
+const todos: Todo[] = []
+
 const inputEl = document.getElementById('input')! as HTMLInputElement
 const todoFormEl = document.getElementById('todoForm')! as HTMLFormElement
 const todoListContainerEl = document.getElementById(
   'todoList'
 ) as HTMLUListElement
 
-const handleSubmit = (e: SubmitEvent) => {
-  e.preventDefault()
-
-  const todoText = inputEl.value
-
+const createTodo = (todo: Todo): void => {
   const todoItemEl = document.createElement('li')
-  todoItemEl.textContent = todoText
+  todoItemEl.textContent = todo.text
 
   const todoItemCheckboxEl = document.createElement('input')
   todoItemCheckboxEl.type = 'checkbox'
 
   todoListContainerEl.appendChild(todoItemEl)
   todoListContainerEl.appendChild(todoItemCheckboxEl)
+}
+
+const handleSubmit = (e: SubmitEvent) => {
+  e.preventDefault()
+
+  const newTodo: Todo = {
+    id: new Date().getTime(),
+    text: inputEl.value,
+    isCompleted: false,
+  }
+
+  createTodo(newTodo)
 
   inputEl.value = ''
 }
